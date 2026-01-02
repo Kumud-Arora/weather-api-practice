cb5ce12a14d2bc64f1274eef1de0e3f9c50f82d0
Kumud Arora <kumudarora11@gmail.com>
2026-01-02 17:04:41 -0600

    Initialize React app structure

--- application/src/App.js
+++ application/src/App.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { use, useState } from 'react';
+import "./styles.css"
+import Header from "./Components/Header"
+import InputCity from './Components/InputCity';
+import ShowWeather from './Components/ShowWeather';
+
 const App = ()=>{
+  const [inputCity, setInputCity] = useState("Seattle")
+  const [cityName, setCityName] = useState("Seattle")
+  const [weatherData, setWeatherData] = useState({});
+  const [error, setError] = useState(false);
+
+  const inputHandler = (e) => {
+    setInputCity(e.target.value)
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setError(false);
+    setCityName(inputCity);
+  }
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid={{api_key_weather_app}}`;
+
+  async function fetchData(URL) {
+    const response = await fetch(URL);
+    const data = await response.json();
+    if (data.cod === "404") {
+      setError(true);
+    } else {
+      setWeatherData(data);
+    }
+  }
+
+  useEffect(() => {
+    fetchData(URL);
+  }, [URL]);
+
   return (
-     <div>Learning with Educative </div>
+    <div>
+     <Header/>
+     <InputCity
+        city = {inputCity}
+        onInputHandler={inputHandler}
+        onsubmitHandler={submitHandler}
+     />
+     <ShowWeather data = {weatherData}/>
+    </div>
   )
 }
 
